430618bdb716b4d6f948f459e1fcea27c90b71cc
playmygamesty <incognito.user562@gmail.com>
2025-07-25 11:22:30 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://51ae51d1-9478-45a8-891a-ba89a4fbb700-00-43larocovmr2.janeway.replit.dev"; // Change to your backend URL
+const API_URL = "https://51ae51d1-9478-45a8-891a-ba89a4fbb700-00-43larocovmr2.janeway.replit.dev/api"; // Change to your backend URL
 
 // Auth helpers
 function getToken() { return localStorage.getItem("token"); }
